ea5405d4ca3c5505979fed6614a23a07fcbf00c6
Rico Chow <rico.ck.chow@gmail.com>
2023-10-26 12:17:52 -0400

    Add form element for each shortURL with POST method. Add POST route for urls/:id/delete to remove URLs

--- express_server.js
+++ express_server.js
@@ -60,7 +60,11 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
