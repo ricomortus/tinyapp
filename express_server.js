56fda670b5b2c70e24ee07c4452e5691bae1f0c0
Rico Chow <rico.ck.chow@gmail.com>
2023-10-26 12:48:14 -0400

    Fix form issue in urls_index for delete button.

--- express_server.js
+++ express_server.js
@@ -62,6 +62,7 @@ app.get("/u/:id", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
+  console.log(id);
   delete urlDatabase[id];
   res.redirect("/urls");
 });
