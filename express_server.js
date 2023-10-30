5a26f5c335836cf51a38c467d71f60ced5ef2674
Rico Chow <rico.ck.chow@gmail.com>
2023-10-30 11:54:52 -0400

    Add POST route for /register and update all endpoints to pass user object instead of username.

--- express_server.js
+++ express_server.js
@@ -22,16 +22,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { };
+
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/news", (req, res) => {
-  res.render("urls_news");
+app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_new",templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -41,7 +46,11 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    user: users[req.cookies["user_id"]]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -64,19 +73,49 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect("/urls");
 });
 
+// app.post("/login", (req, res) => {
+//   const { username } = req.body;
+//   res.cookie("user_id", username);
+//   res.redirect("/urls");
+// });
+
 app.post("/login", (req, res) => {
-  const { username } = req.body;
-  res.cookie("username", username);
-  res.redirect("/urls");
+  const { email } = req.body;
+
+  const userId = Object.keys(users).find(id => users[id].email === email);
+  if (userId) {
+    res.cookie("user_id", userId);
+    res.redirect("/urls");
+  } else {
+    res.status(401).send('Email not registered.');
+  }
 });
 
+
 app.post("/logout", (req,res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 });
 
 app.get("/register", (req, res) => {
-  res.render("urls_register");
+  const templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_register", templateVars);
+});
+
+
+app.post("/register", (req,res) => {
+  const { email, password } = req.body;
+  const id = generateRandomString();
+  users[id] = {
+    id,
+    email,
+    password
+  };
+
+  res.cookie('user_id', id);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
